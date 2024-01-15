0441dec61db4ea8ba7b5204d5df34fdf35d46594
Himanshi Sharma <himanshish456@gmail.com>
2024-01-15 12:36:40 +0530

    route and nested route

---
+++ ch-3/src/app/products/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <>
+      <h1>Products Page</h1>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe in
+        reprehenderit, cum omnis ut, odio aut maiores exercitationem nemo culpa
+        maxime amet. Sapiente ad nemo explicabo, inventore veniam laudantium
+        quibusdam.
+      </p>
+    </>
+  );
+};
+
+export default Products;
