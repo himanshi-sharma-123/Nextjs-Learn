9615284bac36db493a1be7e3481801ad28e961e8
Himanshi Sharma <himanshish456@gmail.com>
2024-01-17 11:51:08 +0530

    url query params

--- ch-4/README.md
+++ ch-4/README.md
@@ -14,3 +14,23 @@ console.log(req)
 
 const requestHeaders = new Headers(req.headers);
 console.log(requestHeaders);
+
+## URL Query Params
+
+Method-1 :
+
+http://localhost:3000/api/products?search=%22java%22
+
+const { searchParams } = new URL(req.url);
+console.log(searchParams);
+console.log(searchParams.get("search"));
+
+output:
+
+URLSearchParams { 'search' => '"java"' }
+"java"
+
+Method-2 :
+
+const searchParams = req.nextUrl.searchParams
+console.log(searchParams.get("search"));
--- ch-4/src/app/api/products/route.js
+++ ch-4/src/app/api/products/route.js
@@ -2,7 +2,19 @@ import { NextResponse } from "next/server";
 
 export async function GET(req) {
   //   console.log(req);
+
+  //Headers
   const requestHeaders = new Headers(req.headers);
-  console.log(requestHeaders);
+  //   console.log(requestHeaders);
+
+  // URL Query Params
+  const { searchParams } = new URL(req.url);
+
+  // or
+
+  //   const searchParams = req.nextUrl.searchParams;
+
+  console.log(searchParams);
+  console.log(searchParams.get("search"));
   return NextResponse.json({ msg: "Hello Nextjs API" });
 }
