7e630062aebd75c63064021f5ca608fcf09ccb84
Himanshi Sharma <himanshish456@gmail.com>
2024-01-19 21:46:04 +0530

    post data using form

--- ch-5/src/app/page.js
+++ ch-5/src/app/page.js
@@ -1,12 +1,12 @@
 import GetData1 from "@/components/GetData1";
+import PostData1 from "@/components/PostData1";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        <GetData1 />
-      </h1>
+      <PostData1 />
+      <GetData1 />
     </main>
   );
 }
